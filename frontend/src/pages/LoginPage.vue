b61d8bc332e98c2b7b0ccf55f0a53ade4e9ca8a2
Haleym Huidalgo Moyetones <109298016+hahidalgo@users.noreply.github.com>
2023-06-27 13:37:45 -0400

    :construction: :sparkles: Mejoa visual al componente

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,64 +1,213 @@
 <template>
-    <div
-      class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
-    >
-      <div class="q-pa-md" style="max-width: 560px">
-        <q-form @submit="onSubmit" class="q-gutter-md">
-          <q-input
-            filled
-            v-model="username"
-            label="Username"
-            lazy-rules
-            :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-          />
-  
-          <q-input
-            filled
-            type="password"
-            v-model="password"
-            label="Password"
-            lazy-rules
-            :rules="[
-              (val) => (val !== null && val !== '') || 'Please type your age',
-            ]"
-          />
-  
-          <div>
-            <q-btn label="Submit" type="submit" color="primary" />
+  <div style="background-color: #666666;">
+    <div class="limiter">
+      <div class="container-login100">
+        <div class="wrap-login100">
+          <form class="login100-form validate-form">
+            <span class="login100-form-title p-b-43">
+              Entrada del sistema
+            </span>
+
+            <q-input outlined v-model="email" label="Email" class="col-xs-12 col-sm-6 col-md-8 q-pa-xs" />
+            <q-input outlined v-model="password" label="Password" :type="isPwd ? 'password' : 'text'"
+              hint="" class="col-xs-12 col-sm-6 col-md-4 q-pa-xs">
+              <template v-slot:append>
+                <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+              </template>
+            </q-input>
             
+
+            <div class="container-login100-form-btn">
+              <q-btn type="submit" label="Guardar" color="primary" />
+              <q-btn push color="white" text-color="primary" label="Push" />
+            </div>
+
+          </form>
+
+          <div class="login100-more" style="background-image: url('./images/bg-01.jpg');">
           </div>
-        </q-form>
+        </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-  import { defineComponent, ref } from "vue";
-  import { useQuasar } from "quasar";
-  
-  export default defineComponent({
-    name: "LoginPage",
-    setup() {
-      const $q = useQuasar();
-      const username = ref("");
-      const password = ref("");
-  
-      function onSubmit() {
-        alert("onSubmit");
-        $q.notify({
-          color: "green-4",
-          textColor: "white",
-          icon: "cloud_done",
-          message: "Submitted",
-        });
-      }
-  
-      return {
-        username,
-        password,
-        onSubmit,
-      };
-    },
-  });
-  </script>
-  
+<script>
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+  name: "LoginPage",
+  data() {
+    return {
+      email: "",
+      password: "",
+      isPwd: true
+    };
+  },
+
+
+});
+</script>
+<style scoped>
+.limiter {
+  width: 100%;
+  margin: 0 auto;
+}
+
+.container-login100 {
+  width: 100%;
+  min-height: 100vh;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  background: #f2f2f2;
+}
+
+
+.wrap-login100 {
+  width: 100%;
+  background: #fff;
+  overflow: hidden;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+  flex-direction: row-reverse;
+
+}
+
+/*==================================================================
+[ login more ]*/
+.login100-more {
+  width: calc(100% - 560px);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  z-index: 1;
+}
+
+.login100-more::before {
+  content: "";
+  display: block;
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.1);
+}
+
+
+
+/*==================================================================
+[ Form ]*/
+
+.login100-form {
+  width: 560px;
+  min-height: 100vh;
+  display: block;
+  background-color: #f7f7f7;
+  padding: 173px 55px 55px 55px;
+}
+
+.login100-form-title {
+  width: 100%;
+  display: block;
+  font-size: 30px;
+  color: #333333;
+  line-height: 1.2;
+  text-align: center;
+}
+
+
+
+/*------------------------------------------------------------------
+[ Button ]*/
+.container-login100-form-btn {
+  width: 100%;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.login100-form-btn {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+  width: 100%;
+  height: 50px;
+  border-radius: 10px;
+  background: #6675df;
+
+  font-family: Montserrat-Bold;
+  font-size: 12px;
+  color: #fff;
+  line-height: 1.2;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+
+  -webkit-transition: all 0.4s;
+  -o-transition: all 0.4s;
+  -moz-transition: all 0.4s;
+  transition: all 0.4s;
+}
+
+.login100-form-btn:hover {
+  background: #333333;
+}
+
+
+
+/*------------------------------------------------------------------
+[ Responsive ]*/
+
+@media (max-width: 992px) {
+  .login100-form {
+    width: 50%;
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+
+  .login100-more {
+    width: 50%;
+  }
+}
+
+@media (max-width: 768px) {
+  .login100-form {
+    width: 100%;
+  }
+
+  .login100-more {
+    display: none;
+  }
+}
+
+@media (max-width: 576px) {
+  .login100-form {
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-top: 70px;
+  }
+}
+</style>
