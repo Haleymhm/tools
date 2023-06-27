6bcd815b901fa74dd6d2bdb9dc098958565a05bc
Haleym Huidalgo Moyetones <109298016+hahidalgo@users.noreply.github.com>
2023-06-27 18:08:41 -0400

    :construction: :sparkles: update search bar desing

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,11 +1,23 @@
 <template>
+  
   <q-layout view="hHh lpR fFf" class="bg-grey-1">
+    
     <q-header elevated class="bg-white text-grey-8" height-hint="64">
+      <q-bar dense class="bg-light-blue-10 text-white">
+      <div>mobi-net</div>
+      <q-icon name="email" />
+      <q-space />
+      <q-icon name="bluetooth" />
+      <q-icon name="signal_wifi_4_bar" />
+      <q-icon name="signal_cellular_4_bar" />
+      <div class="gt-xs">100%</div>
+      <q-icon name="battery_1_bar" class="text-red" />
+      <div>10:00AM</div>
+    </q-bar>
       <q-toolbar class="GNL__toolbar">
         <q-toolbar-title v-if="$q.screen.gt.xs" shrink class="row items-center no-wrap">
           <img alt="Quasar logo" src="~assets/shield.svg" style="width: 50px; height: 50px" />
-          <span class="q-ml-sm">Safe</span>
-          <span class="q-ml-sm">Track</span>
+          <span class="q-ml-sm">Safe Track</span>
         </q-toolbar-title>
 
         <q-btn flat dense round @click="toggleLeftDrawer" aria-label="Menu" icon="menu" class="q-ml-sm" />
@@ -14,53 +26,8 @@
         <q-input class="GNL__toolbar-input" outlined dense v-model="search" color="bg-grey-7 shadow-1"
           placeholder="Search for topics, locations & sources">
           <template v-slot:prepend>
-            <q-icon v-if="search === ''" name="search" />
-            <q-icon v-else name="clear" class="cursor-pointer" @click="search = ''" />
-          </template>
-          <template v-slot:append>
-            <q-btn flat dense round aria-label="Menu" icon="arrow_drop_down">
-              <q-menu anchor="bottom end" self="top end">
-                <div class="q-pa-md" style="width: 400px">
-                  <div class="text-body2 text-grey q-mb-md">
-                    Narrow your search results
-                  </div>
-
-                  <div class="row items-center">
-                    <div class="col-3 text-subtitle2 text-grey">
-                      Exact phrase
-                    </div>
-                    <div class="col-9 q-pl-md">
-                      <q-input dense v-model="exactPhrase" />
-                    </div>
-
-                    <div class="col-3 text-subtitle2 text-grey">Has words</div>
-                    <div class="col-9 q-pl-md">
-                      <q-input dense v-model="hasWords" />
-                    </div>
-
-                    <div class="col-3 text-subtitle2 text-grey">
-                      Exclude words
-                    </div>
-                    <div class="col-9 q-pl-md">
-                      <q-input dense v-model="excludeWords" />
-                    </div>
-
-                    <div class="col-3 text-subtitle2 text-grey">Website</div>
-                    <div class="col-9 q-pl-md">
-                      <q-input dense v-model="byWebsite" />
-                    </div>
-
-                    <div class="col-12 q-pt-lg row justify-end">
-                      <q-btn flat dense no-caps color="grey-7" size="md" style="min-width: 68px" label="Search"
-                        v-close-popup />
-                      <q-btn flat dense no-caps color="grey-7" size="md" style="min-width: 68px" @click="onClear"
-                        label="Clear" v-close-popup />
-                    </div>
-                  </div>
-                </div>
-              </q-menu>
-            </q-btn>
-          </template>
+            <q-icon name="search" />
+          </template> 
         </q-input>
 
         <q-space />
