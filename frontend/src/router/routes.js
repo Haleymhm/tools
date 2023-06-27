dbe08ac4cb67af3576c9ccddfd3fefb29d0c046f
Haleym Huidalgo Moyetones <109298016+hahidalgo@users.noreply.github.com>
2023-06-27 13:05:17 -0400

    :tada: :sparkles: Diseño inicial del componente

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,5 +1,9 @@
 const routes = [
   {
+    path: "/login",
+    component: () => import("pages/LoginPage.vue"),
+  },
+  {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [
